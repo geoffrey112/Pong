40def306f694c5776341dd4c0632eaf0c01f62bf
geoffrey112 <geoffrey.legall@orange.fr>
2024-07-10 10:46:13 +0200

    Upgrade changeColorBtn

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const btnStart = document.getElementById('buttonStart');
-const txtStart = document.querySelector('#buttonStart > span');
-const events = ['pointerdown', 'pointerup', 'mouseover', 'mouseout'];
+const btnStart = document.getElementsByClassName('buttonStart')[0];
+const txtStart = document.querySelector('.buttonStart > span');
+const events = ['pointerdown', 'pointerup', 'mouseover', 'mouseout', 'click'];
 
-const btnKey = document.getElementById('buttonKey');
+const btnKey = document.getElementsByClassName('buttonKey')[0];
 const containKey = document.getElementById('containerKey');
 const iconKey = document.getElementById('iconKey');
 
@@ -10,36 +10,49 @@ const iconKey = document.getElementById('iconKey');
 // Design start & key button
 events.forEach((event) => {
   btnStart.addEventListener(event, (e) => changeColorBtn(e, btnStart, txtStart));
-  btnKey.addEventListener(event, (e) => changeColorBtn(e, btnKey, containKey, iconKey));
+  btnKey.addEventListener(event, function(e){
+    changeColorBtn(e, btnKey, containKey, iconKey);
+    openKeyButton(e);
+  });
 });
 
 
-
 function changeColorBtn(event, elem, txt, icon){
+  // Condition pour key: Si key est fermé
   if(event.type === 'pointerdown'){
-    elem.style.transitionDuration = '0.05s';
-    elem.style.borderColor = '#999999 white white #999999';
-    elem.style.transform = 'translate(0, 0)';
-    elem.style.boxShadow = '0 0 0 0';
+    elem.classList.remove('buttonHover', 'buttonSlow');
+    elem.classList.add('buttonDown', 'buttonFast');
     txt.style.transform = 'translate(2px, 2px)';
   }else if(event.type === 'pointerup'){
-    elem.style.borderColor = 'white #999999 #999999 white';
-    elem.style.transform = 'translate(-2px, -2px)';
-    elem.style.boxShadow = '3px 3px 13px 0 #ffffff9e';
+    elem.classList.remove('buttonDown');
+    elem.classList.add('buttonHover');
     txt.style.transform = 'translate(0, 0)';
   }else if(event.type === 'mouseover'){
-    elem.style.borderColor = 'white #999999 #999999 white';
-    elem.style.boxShadow = '3px 3px 13px 0 #ffffff9e';
-    elem.style.transform = 'translate(-2px, -2px)';
+    elem.classList.add('buttonHover', 'buttonSlow');
+    elem.classList.remove('buttonFast');
     icon ? icon.style.background = 'center/cover url(icon/arcadeBlack.png)': null;
   }else if(event.type === 'mouseout'){
-    elem.style.transitionDuration = '0.5s';
-    elem.style.borderColor = 'white';
-    elem.style.boxShadow = '0 0 0 0';
-    elem.style.transform = 'translate(0, 0)';
+    elem.classList.remove('buttonHover', 'buttonDown', 'buttonFast');
+    elem.classList.add('buttonSlow');
     txt.style.transform = 'translate(0, 0)';
     icon ? icon.style.background = 'center/cover url(icon/arcadeWhite.png)': null;
+  }else if(event.type === 'click'){
+    // Condition pour start game: Si les inputs ne sont pas vide
+    elem.classList.remove('buttonHover');
+    txt.style.display = 'none';
   }
 }
+
+
+function openKeyButton(event){
+  if(event.type === "click"){
+    
+  }
+
+}
+
+
+
+
 
 
