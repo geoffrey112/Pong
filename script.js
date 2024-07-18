a604fecd57a8ceca24a80e79c99a404bdaf200de
geoffrey112 <geoffrey.legall@orange.fr>
2024-07-18 14:47:26 +0200

    upgrade open/close keyBtn + add errorMsg to checkInput

--- script.js
+++ script.js
@@ -7,80 +7,88 @@ const btnKey = document.getElementsByClassName('buttonKey')[0];
 const containKey = document.getElementById('containerKey');
 const iconKey = document.getElementById('iconKey');
 let isKeyOpen = false;
+let isKeyMoving = false;
 
 const inputJ1 = document.getElementsByName('j1')[0];
 const inputJ2 = document.getElementsByName('j2')[0];
 
-
-// Design start & key button
+// Design startButton & keyButton
 events.forEach((event) => {
   btnStart.addEventListener(event, function(e){
     changeColorBtn(e, btnStart, txtStart);
-    checkInput(e);
   });
-
+  
   btnKey.addEventListener(event, function(e){
-    if(isKeyOpen === false){
-      changeColorBtn(e, btnKey, containKey, iconKey);
-      openKeyBtn(e);
-    }
+    changeColorBtn(e, btnKey, containKey, iconKey);
   });
 });
+
+// Open / Close KeyButton
+btnKey.addEventListener('click', openKeyBtn);
 
 document.addEventListener('click', function(e){
-  if(isKeyOpen === true){
-    closeKeyBtn(e);
-  }
+  closeKeyBtn(e);
 });
+
+// Start Game
+btnStart.addEventListener('click', function(){
+  checkInput();
+});
+
 
 
 function changeColorBtn(event, elem, txt, icon){
-  if(event.type === 'pointerdown'){
-    elem.classList.remove('buttonHover', 'buttonSlow');
-    elem.classList.add('buttonDown', 'buttonFast');
-    txt.style.transform = 'translate(2px, 2px)';
-  }else if(event.type === 'pointerup'){
-    elem.classList.remove('buttonDown');
-    elem.classList.add('buttonHover');
-    txt.style.transform = 'translate(0, 0)';
-  }else if(event.type === 'mouseover'){
-    elem.classList.add('buttonHover', 'buttonSlow');
-    elem.classList.remove('buttonFast');
-    icon ? icon.style.background = 'center/cover url(icon/arcadeBlack.png)': null;
-    containKey.classList.remove('containKeyIn');
-  }else if(event.type === 'mouseout'){
-    elem.classList.remove('buttonHover', 'buttonDown', 'buttonFast');
-    elem.classList.add('buttonSlow');
-    txt.style.transform = 'translate(0, 0)';
-    icon ? icon.style.background = 'center/cover url(icon/arcadeWhite.png)': null;
-    containKey.classList.remove('containKeyIn');
-  }else if(event.type === 'click'){
-    icon ? icon.style.background = 'center/cover url(icon/arcadeWhite.png)': null;
+
+  if(isKeyOpen === false && isKeyMoving === false){
+    if(event.type === 'pointerdown'){
+      elem.classList.remove('buttonHover', 'buttonSlow');
+      elem.classList.add('buttonDown', 'buttonFast');
+      txt.style.transform = 'translate(2px, 2px)';
+    }else if(event.type === 'pointerup'){
+      elem.classList.remove('buttonDown');
+      elem.classList.add('buttonHover');
+      txt.style.transform = 'translate(0, 0)';
+    }else if(event.type === 'mouseover'){
+      elem.classList.add('buttonHover', 'buttonSlow');
+      elem.classList.remove('buttonFast');
+      icon ? icon.style.background = 'center/cover url(icon/arcadeBlack.png)': null;
+      containKey.classList.remove('containKeyIn');
+    }else if(event.type === 'mouseout'){
+      elem.classList.remove('buttonHover', 'buttonDown', 'buttonFast');
+      elem.classList.add('buttonSlow');
+      txt.style.transform = 'translate(0, 0)';
+      icon ? icon.style.background = 'center/cover url(icon/arcadeWhite.png)': null;
+      containKey.classList.remove('containKeyIn');
+    }else if(event.type === 'click'){
+      icon ? icon.style.background = 'center/cover url(icon/arcadeWhite.png)': null;
+    }
+
   }
+
 }
 
 
-function openKeyBtn(event){
+function openKeyBtn(){
 
-  const containCardKey = document.createElement('div');
-  const cardKey1 = document.createElement('div');
-  const cardKey2 = document.createElement('div');
-  const titleJ1 = document.createElement('h2');
-  const titleJ2 = document.createElement('h2');
-  const keyZ = document.createElement('div');
-  const keyS = document.createElement('div');
-  const keyUp = document.createElement('div');
-  const keyDown = document.createElement('div');
-
+  if(isKeyOpen === false){
+    const containCardKey = document.createElement('div');
+    const cardKey1 = document.createElement('div');
+    const cardKey2 = document.createElement('div');
+    const titleJ1 = document.createElement('h2');
+    const titleJ2 = document.createElement('h2');
+    const keyZ = document.createElement('div');
+    const keyS = document.createElement('div');
+    const keyUp = document.createElement('div');
+    const keyDown = document.createElement('div');
   
-  if(event.type === "click"){
+  
     btnKey.classList.remove('buttonHover', 'buttonFast');
-    setTimeout(() => { containKey.classList.add('containKeyOut'); }, 10);
-
+    setTimeout(() => { containKey.classList.add('containKeyOut'); }, 10); // Short delay to work
+  
     btnKey.classList.remove('closeKey');
     btnKey.classList.add('openKey');
     containCardKey.classList.add('containCardKey');
-    setTimeout(() => { containCardKey.classList.add('cardKeyIn'); }, 10); // Short delay
+    setTimeout(() => { containCardKey.classList.add('cardKeyIn'); }, 10); // Short delay to work
     cardKey1.classList.add('cardKey1');
     cardKey2.classList.add('cardKey2');
     titleJ1.classList.add('titleKeyJ1');
@@ -99,9 +107,10 @@ function openKeyBtn(event){
     containCardKey.append(cardKey1, cardKey2);
     cardKey1.append(titleJ1, keyZ, keyS);
     cardKey2.append(titleJ2, keyUp, keyDown);
-
     isKeyOpen = true;
+    isKeyMoving = true;
   }
+
 }
 
 
@@ -109,32 +118,37 @@ function closeKeyBtn(event){
 
   const containCardKey = document.getElementsByClassName('containCardKey')[0];
 
-  if(!event.target.closest('div.buttonKey')){
-    containCardKey.classList.add('cardKeyOut');
-    setTimeout(() => {
-      containCardKey.remove();
-      btnKey.classList.remove('openKey');
-      btnKey.classList.add('closeKey');
-    },500);
-    setTimeout(() => {
-      containKey.classList.remove('containKeyOut');
-      containKey.classList.add('containKeyIn');
-    }, 1000);
-    setTimeout(() => {
-      btnKey.classList.remove('closeKey');
-    }, 1500);
+  if(isKeyOpen === true){
+    if(!event.target.closest('div.buttonKey')){
+      containCardKey.classList.add('cardKeyOut');
+      containKey.classList.replace('containKeyOut', 'containKeyIn');
+      isKeyMoving = true;
 
-    isKeyOpen = false;
+      setTimeout(() => {
+        containCardKey.remove();
+        btnKey.classList.replace('openKey', 'closeKey');
+      },500);
+      setTimeout(() => {
+        btnKey.classList.remove('closeKey');
+        isKeyMoving = false;
+      }, 1500);
+
+      isKeyOpen = false;
+
+    }
   }
 
 }
 
 
-function checkInput(event){
+function checkInput(){
 
   const errorMsg = document.getElementById('errorMsg');
+  const findSpace = /\s/g;
 
-  const animMsg = () => {
+  console.log(findSpace.test(inputJ1.value));
+
+  const animErrorMsg = () => {
     errorMsg.animate([
       {transform: 'rotateZ(5deg)'},
       {transform: 'rotateZ(-5deg)'},
@@ -145,20 +159,28 @@ function checkInput(event){
     });
   }
 
-  if(event.type === 'click'){
-    if((inputJ1.value === '' || inputJ1.value === ' ') && (inputJ2.value === '' || inputJ2.value === ' ')){
-      errorMsg.textContent = "Enter player 1 & 2";
-      animMsg();
-    }else if(inputJ1.value === '' || inputJ1.value === ' '){
-      errorMsg.textContent = "Enter player 1";
-      animMsg();
-    }else if(inputJ2.value === '' || inputJ2.value === ' '){
-      errorMsg.textContent = "Enter player 2";
-      animMsg();
-    }else if(inputJ1.value !== '' || inputJ1.value !== ' ' && inputJ2.value !== '' || inputJ2.value !== ' '){
-      errorMsg.textContent = "";
-    }
+  const disableSpace = (e) => {
+
   }
+
+  const enableSpace = () => {
+    
+  }
+
+
+  if((inputJ1.value === '' || inputJ1.value === ' ') && (inputJ2.value === '' || inputJ2.value === ' ')){
+    errorMsg.textContent = "Enter player 1 & 2";
+    animErrorMsg();
+  }else if(inputJ1.value === '' || inputJ1.value === ' '){
+    errorMsg.textContent = "Enter player 1";
+    animErrorMsg();
+  }else if(inputJ2.value === '' || inputJ2.value === ' '){
+    errorMsg.textContent = "Enter player 2";
+    animErrorMsg();
+  }else if(inputJ1.value !== '' || inputJ1.value !== ' ' && inputJ2.value !== '' || inputJ2.value !== ' '){
+    errorMsg.textContent = "";
+  }
+  
 };
 
 
